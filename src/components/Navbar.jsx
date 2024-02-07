25cd85b7b3e8b31f5ede4aff221944835ba6eea5
Ricardo_Li <46476463+RicardoYam@users.noreply.github.com>
2024-02-07 20:12:14 +1000

    Navbar completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,37 +2,39 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './Navbar.css'
 import { IoMdMenu, IoMdClose } from "react-icons/io";
-import { useState, useRef } from 'react';
+import { useRef } from 'react';
 
 function nav() {
-  const navRef = useRef()
+  const navRef = useRef();
 
   const showNavbar = () => {
-    navRef.current.classList.toggle('responsive_nav')
-  }
+    navRef.current.classList.toggle(
+      "responsive_nav"
+    );
+  };
 
   return (
-    <>
-      <div className='nav'>
-        <a className='nav-logo'><Link to={'/'}>SHIXUN'S</Link></a>
-        <button className='nav-btn nav-btn-menu'>
-          <IoMdMenu style={{ color: "white", marginTop: "5px" }} />
-        </button>
-        <nav ref={navRef}>
-          <ul>
-            <li><Link to={'/projects'}>Projects</Link></li>
-            <li><Link to={'/about'}>About</Link></li>
-            <li><Link to={'/contact'}>Contact</Link></li>
-            <li>
-              <button className='nav-btn nav-btn-close' onClick={showNavbar}>
-                <IoMdClose style={{ color: "white", marginTop: "5px" }} />
-              </button>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </>
-  )
+    <header>
+      <span>SHIXUN</span>
+      <button
+        className="nav-btn"
+        onClick={showNavbar}>
+        <IoMdMenu />
+      </button>
+      <nav ref={navRef}>
+        <ul>
+          <li>
+            <button className='nav-btn nav-close-btn' onClick={showNavbar}>
+              <IoMdClose/>
+            </button>
+          </li>
+          <li><Link to={'/projects'}>Projects</Link></li>
+          <li><Link to={'/about'}>About</Link></li>
+          <li><Link to={'/contact'}>Contact</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default nav
