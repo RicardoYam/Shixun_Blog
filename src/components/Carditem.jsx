b50dbd4975ef9cf500a7bd8c3693de98492b4b69
Ricardo_Li <46476463+RicardoYam@users.noreply.github.com>
2024-02-08 15:49:10 +1000

    introduction added on root page

---
+++ src/components/Carditem.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Carditem() {
+  return (
+    <div>Carditem</div>
+  )
+}
+
+export default Carditem
--- src/pages/root.jsx
+++ src/pages/root.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import './root.css'
 import { IoIosArrowRoundForward } from "react-icons/io";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+import { SiLeetcode } from "react-icons/si";
+import Cards from '../components/Cards';
 
 function root() {
   return (
@@ -23,9 +26,16 @@ function root() {
             <div className='content-contact-item'>
               <p>Contact:</p>
               <p>lishixun1219@gmail.com</p>
+              <a href="https://www.linkedin.com/in/shixun-li-669a4b287/"><FaLinkedin style={{ color: 'white', marginTop: '10px', marginRight: '10px' }} size='30px' /></a>
+              <a href="https://github.com/RicardoYam"><FaGithub style={{ color: 'white', marginTop: '10px', marginRight: '10px' }} size='30px' /></a>
+              <a href="https://leetcode.com/RicardoOVO/"><SiLeetcode style={{ color: 'white', marginTop: '10px' }} size='30px' /></a>
             </div>
           </div>
         </div>
+        <div className='content-intro'>
+          <video src="src\assets\videos\fashion.mp4" autoPlay loop muted></video>
+        </div>
+        <Cards />
       </div>
     </>
   )
