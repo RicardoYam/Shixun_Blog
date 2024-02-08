fdf70ed0b27e27ca660916c87ebb6d9ea84e0290
Ricardo_Li <46476463+RicardoYam@users.noreply.github.com>
2024-02-08 13:44:29 +1000

    Banner updated with responsive features

--- src/pages/root.jsx
+++ src/pages/root.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
-import Gallery from '../components/Gallery'
+import './root.css'
+import { IoIosArrowRoundForward } from "react-icons/io";
 
 function root() {
   return (
     <>
       <div className='header'><Navbar /></div>
       <div className='content'>
-        <Gallery />
+        <div className="banner">
+          <h1>Hello,</h1>
+          <h1>Nice to meet you</h1>
+          <div className='content-contact'>
+            <IoIosArrowRoundForward style={{ color: 'white', margin: '30px' }} size='130px' />
+            <div className='content-contact-item'>
+              <p>Front-end &</p>
+              <p>Back-end development</p>
+            </div>
+            <div className='content-contact-item'>
+              <p>Data Analysis</p>
+            </div>
+            <div className='content-contact-item'>
+              <p>Contact:</p>
+              <p>lishixun1219@gmail.com</p>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   )
