703315f815b5cc6547b74446012db948c59628b8
Ricardo_Li <46476463+RicardoYam@users.noreply.github.com>
2024-02-12 22:54:55 +1000

    download function completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Root from './pages/root.jsx'
 import Projects from './pages/projects.jsx'
+import Contact from './pages/contact.jsx'
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Root />}></Route>
           <Route path='/projects' element={<Projects />}></Route>
+          <Route path='/contact' element={<Contact />}></Route>
         </Routes>
       </div>
     </>
