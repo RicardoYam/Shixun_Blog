af62a9d5cb1d61813240559ec3d7fff857dcb942
Ricardo_Li <46476463+RicardoYam@users.noreply.github.com>
2024-02-12 17:39:24 +1000

    server connect to Mongodb successfully

--- server/server.js
+++ server/server.js
@@ -1,8 +1,17 @@
+// require('dotenv').config()
+
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 
-app.get("/test", (req, res) => {
-    res.json({"users": ["user1", "user2"]})
-})
+mongoose.connect("mongodb://127.0.0.1:27017/shixun_blog")
+const db = mongoose.connection
+db.on('error', (error) => console.log(error))
+db.once('open', () => console.log('Connected to database'))
 
-app.listen(5000, () => {console.log("server started on port 5000")})
+app.use(express.json())
+
+const blogsRouter = require('./routes/blogs')
+app.use('/blogs', blogsRouter)
+
+app.listen(3000, () => {console.log("server started on port 3000")})
